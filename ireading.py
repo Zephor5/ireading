3a68e6f1ae4df07a47028b45ab178cec72f8c753
unknown <zhihui9@zhihui9.staff.sina.com.cn>
2015-06-09 23:03:22 +0800

    first commit
    Signed-off-by: zephor5

---
+++ ireading.py
@@ -0,0 +1,122 @@
+#coding=utf-8
+import functools
+import json
+import re
+import tornado.ioloop
+import tornado.web
+
+import requests
+from weibo import APIClient
+from constant import APP_ID, APP_SECRET, VALID_POS_TAG
+
+
+def pre_proc(text):
+    try:
+    # UCS-4
+        high_points = re.compile(u'[\U00010000-\U0010ffff]')
+    except re.error:
+        # UCS-2
+        high_points = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
+    link = re.compile(r'http://t\.cn/\w{7}')
+    emotions = re.compile(r'\[.{2,8}\]')
+    text = emotions.sub('', text)
+    text = high_points.sub('', text)
+    text = link.sub('', text)
+    return text
+
+
+def _create_client(oauth_token=None, expires=None):
+    client = APIClient(APP_ID, APP_SECRET, 'http://anid.sinaapp.com/callback')
+    if oauth_token and expires:
+        client.set_access_token(oauth_token, expires)
+    return client
+
+
+def auth_check(func):
+    @functools.wraps(func)
+    def wrap(self, *args, **kwargs):
+        signed_request = self.get_argument("signed_request", "null")
+        client = _create_client()
+        data = client.parse_signed_request(signed_request)
+        if data is not None:
+            uid = data.get('uid', '')
+            auth_token = data.get('oauth_token', '')
+            if uid and auth_token:
+                expires = data.expires
+                client.set_access_token(auth_token, expires)
+                kwargs['client'] = client
+                kwargs['uid'] = uid
+                kwargs['signed_request'] = signed_request
+                return func(self, *args, **kwargs)
+        return self.write(APP_ID)
+
+    return wrap
+
+
+def get_pos(word, words):
+    for i, _w in enumerate(words):
+        if _w['word'] == word['word']:
+            return i
+    return -1
+
+
+def get_sem(text):
+    res = requests.post('http://anid.sinaapp.com/fenci',
+                        {'context': text})
+    res = json.loads(res.content)
+    words = []
+    for word in res:
+        if word['word_tag'] in VALID_POS_TAG:
+            p = get_pos(word, words)
+            if p == -1:
+                word['rate'] = 1
+                words.append(word)
+            else:
+                words[p]['rate'] += 1
+    return words
+
+
+def get_book(q):
+    res = requests.get('https://api.douban.com/v2/book/search?q=%s&count=10' % q)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    @auth_check
+    def get(self, *args, **kwargs):
+        client = kwargs['client']
+        uid = kwargs['uid']
+
+        data = client.statuses.home_timeline.get(count=20, page=1, feature=1, trim_user=1)
+
+        till_id = data['next_cursor']
+
+        context = []
+        print len(data['statuses'])
+        for status in data['statuses']:
+            text = pre_proc(status['text'])
+            print text
+            context.append(text)
+
+        # 分词
+        words = get_sem("\n".join(context))
+
+        #过滤
+        # words = filter(lambda word: word['rate'] > 2, words)
+
+        #排序
+        words = sorted(words, key=lambda x: x['rate'], reverse=True)
+
+        #搜索
+
+        self.write(json.dumps(words))
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(13982)
+    tornado.ioloop.IOLoop.instance().start()
+    # print get_sem('这是测试')
+    # pass
